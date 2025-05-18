aabf6ed8e12c3d5269d5ce09e4334840a8ec9c55
solal-adamowicz <solal.adamowicz@outlook.fr>
2025-05-19 00:35:03 +0200

    don't use brew for osmium

--- backend/graphhopper-data/osm/filter-osm.bash
+++ backend/graphhopper-data/osm/filter-osm.bash
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-
-brew install osmium-tool
-
 wget https://download.geofabrik.de/europe/france/ile-de-france-latest.osm.pbf
 osmium tags-filter -R ile-de-france-latest.osm.pbf -o ile-de-france-foot-transit.osm.pbf -i osm-filters.json
