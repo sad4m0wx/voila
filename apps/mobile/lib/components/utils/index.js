aaf6129851af50ca19f22fc876d5a2051c5a5725
solal-adamowicz <solal.adamowicz@outlook.fr>
2025-07-04 19:24:55 +0200

    delete obsolete addmembercomponent (replaced by address input)

--- apps/mobile/lib/components/utils/index.js
+++ apps/mobile/lib/components/utils/index.js
@@ -1,4 +1,3 @@
 export { default as LoadingIndicator } from './LoadingIndicator';
 export { default as SlideToConfirm } from './SlideToConfirm';
 export { default as BottomSheet } from './BottomSheet';
-export { AddMemberComponent } from './AddMemberComponent'; 
