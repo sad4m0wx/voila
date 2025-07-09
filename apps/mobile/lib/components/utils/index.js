5eed914b5d71f6ac2604ab863dc2d05d5d0fe121
solal-adamowicz <solal.adamowicz@outlook.fr>
2025-07-09 15:28:34 +0200

    delete obsolete bottomsheet

--- apps/mobile/lib/components/utils/index.js
+++ apps/mobile/lib/components/utils/index.js
@@ -1,3 +1,2 @@
 export { default as LoadingIndicator } from './LoadingIndicator';
 export { default as SlideToConfirm } from './SlideToConfirm';
-export { default as BottomSheet } from './BottomSheet';
